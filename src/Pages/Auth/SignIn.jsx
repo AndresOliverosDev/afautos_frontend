01d84dc310085dbf143272e0b881125f9df0f622
Andres Felipe Oliveros Anacona <andres.oliverosaf@gmail.com>
2024-02-13 18:00:39 -0500

    Form Authentication Design

--- src/Pages/Auth/SignIn.jsx
+++ src/Pages/Auth/SignIn.jsx
@@ -2,7 +2,7 @@ import AuthForm from "./AuthForm";
 
 const SignIn = () => {
     return (
-        <div className="">
+        <div className="bg-gradient-to-br from-black via-teal-700 to-cyan-400">
             <AuthForm title='Iniciar Sesion'/>
         </div>
     );
--- src/components/ui/FormInput.jsx
+++ src/components/ui/FormInput.jsx
@@ -3,7 +3,7 @@ const FormInput = ({ icon: Icon, ...props }) => {
         <div className="flex items-center relative">
             {Icon && <Icon className="text-gray-300 w-6 h-6 absolute left-3 top-1/2 transform -translate-y-1/2" />}
             <input
-                className='focus:outline-color-primary-dark bg-gray-600 rounded-md py-2 px-11 text-gray-200'
+                className='border-solid border border-red-900 focus:border-color-primary-dark bg-gray-600 rounded-md py-2 px-11 text-gray-200'
                 {...props}
             />
         </div>
