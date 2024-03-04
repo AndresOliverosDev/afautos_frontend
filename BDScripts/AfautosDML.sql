675bae19498699e770b6c8fe51a7afa3087d72c4
Andres Felipe Oliveros Anacona <andres.oliverosaf@gmail.com>
2024-03-03 23:30:54 -0500

    modify ddl mysql

---
+++ BDScripts/AfautosDML.sql
@@ -0,0 +1,9 @@
+/*  Rol de usuarios   */
+INSERT RolUsuar(nomRol)
+    VALUES ("Cliente"),("Proveedor"),("Administrador"),("Vendedor"),("Empleado operativo");
+
+/*  Tipos de documento  */
+INSERT TipDoc(nomDoc)
+    VALUES ("Cédula de ciudadanía"),("Cédula de extranjería"),("NIT");
+
+/*  Marcas de productos  */
--- src/components/ui/FormInput.jsx
+++ src/components/ui/FormInput.jsx
@@ -1,27 +1,18 @@
 import { useState } from "react";
 
 const FormInput = ({ icon: Icon, ...props }) => {
-  const [focus, setFocus] = useState(false);
-  const handleFocus = () => {
-    setFocus((prevState) => !prevState);
-  };
-  return (
-    <div
-      onFocus={handleFocus}
-      onBlur={handleFocus}
-      className="relative flex items-center"
-    >
-      {Icon && (
-        <Icon
-          className={`transition-all duration-300 ${focus ? "text-color-primary-light" : "text-gray-300"}  absolute left-3 top-1/2 h-6 w-6 -translate-y-1/2 transform`}
-        />
-      )}
-      <input
-        className="rounded-md border border-black border-opacity-0 bg-black bg-opacity-60 px-11 py-2 text-gray-200 transition-all duration-500 focus:border-color-primary-dark"
-        {...props}
-      />
-    </div>
-  );
-};
+    const [focus, setFocus] = useState(false);
+    const handleFocus = ()=> {
+        setFocus((prevState)=> !prevState );
+    }
+    return (
+        <div onFocus={handleFocus} onBlur={handleFocus}  className="flex items-center relative">
+            {Icon && <Icon className={`transition-all duration-300 ${focus ? 'text-color-primary-light' : 'text-gray-300'} w-6 h-6 absolute left-3 top-1/2 transform -translate-y-1/2`} />}
+            <input className='transition-all duration-500 border border-black border-opacity-0 focus:border-color-primary-dark bg-black bg-opacity-60 rounded-md py-2 px-11 text-gray-200'
+                {...props}
+            />
+        </div>
+    );
+}
 
 export default FormInput;
